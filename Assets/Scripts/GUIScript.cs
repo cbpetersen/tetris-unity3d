72d8d16c7164927210925b21fd35704b029130e2
Christoffer Petersen <cb.petersen@gmail.com>
2016-05-06 20:49:49 +0200

    More clean up of the gui script

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -1,6 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Linq;
 
 // ReSharper disable once InconsistentNaming
 // ReSharper disable once CheckNamespace
@@ -18,9 +16,6 @@ public class GUIScript : MonoBehaviour
     private int block = 0;
     private float blockSpawned = 1;
     private float avgBlockPerMin;
-    private LineRenderer lineRenderer;
-    private readonly ArrayList evalList = new ArrayList();
-    private Vector2 scrollViewVector = Vector2.zero;
     private static float speedHSliderValue = 1;
     private static float blockHSliderValue;
     private float avgRowsPerMin;
@@ -35,22 +30,22 @@ public class GUIScript : MonoBehaviour
     private Rect avgRowRect;
     private Rect blockRect;
     private Rect avgBlockRect;
-    private Rect speedRect;
+    private Rect speedLabelRect;
     private Rect autoplayRect;
     private float labelLeftMargin;
     private bool gameover;
     private Rect creditwwwRect;
     private Rect creditRect;
     private static bool autoplayBool = true;
+    private Rect gameoverRect;
+    private Rect speedRect;
+    private Rect statsBoxRect;
+    private Rect settingsBoxRect;
 
     // ReSharper disable once UnusedMember.Local
     private void Start()
     {
-        this.lineRenderer = this.GetComponent<LineRenderer>();
-        for (var i = 0; i < 100; i++)
-        {
-            this.evalList.Add(0.0f);
-        }
+        this.startTime = Time.time;
 
         if (Screen.width == 600)
         {
@@ -74,10 +69,15 @@ public class GUIScript : MonoBehaviour
         this.avgRowRect = new Rect(this.labelLeftMargin, 160, labelwidth, labelHieght);
         this.blockRect = new Rect(this.labelLeftMargin, 180, labelwidth, labelHieght);
         this.avgBlockRect = new Rect(this.labelLeftMargin, 200, labelwidth, labelHieght);
-        this.speedRect = new Rect(this.labelLeftMargin, 260, labelwidth, labelHieght);
+        this.speedRect = new Rect(Screen.width / 3 - 160, 265, 95, 30);
+        this.speedLabelRect = new Rect(this.labelLeftMargin, 260, labelwidth, labelHieght);
         this.autoplayRect = new Rect(this.labelLeftMargin, 310, labelwidth, labelHieght);
         this.creditRect = new Rect(this.labelLeftMargin, 350, labelwidth, labelHieght);
+        this.gameoverRect = new Rect(Screen.width / 2 - 100, Screen.height / 2f, 200, 30);
         this.creditwwwRect = new Rect(this.labelLeftMargin, 370, labelwidth, labelHieght);
+
+        this.statsBoxRect = new Rect(this.labelLeftMargin - 10, 10, 200, 220);
+        this.settingsBoxRect = new Rect(this.labelLeftMargin - 10, 260, 200, 220);
     }
 
     // ReSharper disable once UnusedMember.Local
@@ -89,18 +89,11 @@ public class GUIScript : MonoBehaviour
 
         if (this.gameover)
         {
-            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2f, 200, 30), "Gameover starting over soon");
+            GUI.Label(this.gameoverRect, "Gameover starting over soon");
         }
 
-        if (autoplayBool)
-        {
-            autoplayBool = GUI.Toggle(this.autoplayRect, autoplayBool, "Auto play ON");
-        }
-        else
-        {
-            autoplayBool = GUI.Toggle(this.autoplayRect, autoplayBool, "Auto play OFF");
-        }
-
+        GUI.Box(this.statsBoxRect, string.Empty);
+        autoplayBool = GUI.Toggle(this.autoplayRect, autoplayBool, string.Format("Auto play {0}", autoplayBool ? "ON" : "OFF"));
         this.avgRowsPerMin = this.rowscleared / this.time * 60;
         GUI.Label(this.timeRunningRect, "Time Running: " + PrettyTime((int)this.time));
         GUI.Label(this.oneRowRect, "1 Rows: " + this.oneRowsCleared);
@@ -109,15 +102,13 @@ public class GUIScript : MonoBehaviour
         GUI.Label(this.fourRowRect, "4 Rows: " + this.fourRowsCleared);
         GUI.Label(this.totalRowRect, "Total Rows Cleared: " + this.rowscleared);
         GUI.Label(this.fitnessRowRect, "Fitness: " + this.fitness);
-
         GUI.Label(this.avgBlockRect, "Avg Blocks per min: " + (int)this.avgBlockPerMin);
         GUI.Label(this.blockRect, "Blocks spawned: " + this.blockSpawned);
         GUI.Label(this.avgRowRect, "Avg rows per min: " + (int)this.avgRowsPerMin);
-        speedHSliderValue = GUI.HorizontalSlider(new Rect(Screen.width / 3 - 160, 265, 95, 30), speedHSliderValue, 1.0f, 1000.0f);
-        GUI.Label(this.speedRect, "Speed: " + (int)speedHSliderValue);
-        GUI.Box(new Rect(this.labelLeftMargin - 10, 260, 200, 220), string.Empty);
-        GUI.Box(new Rect(this.labelLeftMargin - 10, 10, 200, 220), string.Empty);
 
+        GUI.Box(this.settingsBoxRect, string.Empty);
+        speedHSliderValue = GUI.HorizontalSlider(this.speedRect, speedHSliderValue, 1.0f, 1000.0f);
+        GUI.Label(this.speedLabelRect, "Speed: " + (int)speedHSliderValue);
         GUI.Label(this.creditRect, "\u00a9 Christoffer Bo Petersen");
         GUI.Label(this.creditwwwRect, "www.cb-p.dk");
     }
@@ -134,11 +125,6 @@ public class GUIScript : MonoBehaviour
 
     public void UpdateScore(Tetris.Engine.GameStats gameResult)
     {
-        if (this.startTime == 0)
-        {
-            this.startTime = Time.time;
-        }
-
         this.rowscleared = gameResult.TotalRowClearings;
         this.fitness = gameResult.Fitness;
         this.oneRowsCleared = gameResult.OneRowClearings;
@@ -148,35 +134,15 @@ public class GUIScript : MonoBehaviour
         this.blockSpawned = gameResult.BlocksSpawned;
     }
 
-    public void SetEval(float eval)
-    {
-        this.evalList.RemoveAt(0);
-        this.evalList.Add(eval);
-    }
-
     public float GetGameSpeed()
     {
         return speedHSliderValue;
     }
 
-    private int GetNumberofLines(string str)
-    {
-        return str.Count(x => x == '\n');
-    }
-
     private static string PrettyTime(int time)
     {
         var sec = time % 60;
-        int min;
-        if (time > 3600)
-        {
-            min = time % 3600 / 60;
-        }
-        else
-        {
-            min = time / 60;
-        }
-
+        var min = time % 3600 / 60;
         var hour = time / 3600;
 
         return string.Format(
